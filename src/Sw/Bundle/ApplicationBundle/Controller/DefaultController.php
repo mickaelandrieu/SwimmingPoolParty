1128c8b964b68a422d87c6e8161e511fd868cc60
mickaelandrieu <andrieu.travail@gmail.com>
2014-06-11 09:36:29 +0200

    Added fake action to get one swimming pool

--- src/Sw/Bundle/ApplicationBundle/Controller/DefaultController.php
+++ src/Sw/Bundle/ApplicationBundle/Controller/DefaultController.php
@@ -21,7 +21,7 @@ class DefaultController extends Controller
     /**
      * @deprecated This action should be removed when REST Service will be available
      *
-     * @return JSonResponse The list of Swimming pools
+     * @return JSonResponse The list of Swimmingpools
      */
     public function getSwimmingPoolsAction()
     {
@@ -36,6 +36,28 @@ class DefaultController extends Controller
         ;
 
         return $response->setData(array('swimmingpools' => $swimmingpools));
+    }
+
+    /**
+     * @deprecated This action should be removed when REST Service will be available
+     *
+     * @param id The Swimmingpool identifier
+     * @return JsonResponse The Swimmingpool
+     */
+    public function getSwimmingPoolAction($id)
+    {
+        $response = new JsonResponse();
+        $swimmingpool = $this
+            ->getDoctrine()
+            ->getRepository('SwRestSwimmingBundle:SwimmingPool')
+            ->createQueryBuilder('s')
+            ->where('s.id = :id')
+            ->setParameter('id', $id)
+            ->getQuery()
+            ->getArrayResult()
+        ;
+
+        return $response->setData(array('swimmingpool' => $swimmingpool));
     }
 
     /**
