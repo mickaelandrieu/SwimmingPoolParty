f06087c5a66d84a163080714886e6e7f9f91b6ef
Mickaël Andrieu <andrieu.travail@gmail.com>
2014-06-22 17:53:38 +0200

    Added addComment SOAP API + Action in SoapController

--- src/Sw/Bundle/ApplicationBundle/Controller/SoapClientController.php
+++ src/Sw/Bundle/ApplicationBundle/Controller/SoapClientController.php
@@ -32,6 +32,28 @@ class SoapClientController extends Controller
 
     public function addCommentAction(Request $request)
     {
-        return new Response('addComment');
+        // from the front, your form should at list get this parameters
+        // respect this order in the parameters array, very important
+        $params = array(
+            'swimmingPoolId' => 2919,
+            'author' => 'Mickaël Andrieu',
+            'content' => 'BOUYAKA',
+            'rank' => 1
+        );
+
+        try {
+                $client = new SoapClient(self::WSDL, array('cache_wsdl' => WSDL_CACHE_NONE,
+                    'trace' => true,
+                    'exceptions' => true,
+                    'encoding'=>'UTF-8',
+                    'features' => SOAP_SINGLE_ELEMENT_ARRAYS
+                    )
+                );
+
+                $response = $client->__soapCall('addComment', $params);
+            } catch (\SoapFault $e) {
+                var_dump($e->getMessage(), $client->__getLastResponse());
+        }
+        return new Response($client->__getLastResponse());
     }
 }
