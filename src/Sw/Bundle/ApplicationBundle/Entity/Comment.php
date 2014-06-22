d084b1704c622de8ef4d5508b45d1a067439c2d9
Mickaël Andrieu <andrieu.travail@gmail.com>
2014-06-22 18:55:18 +0200

    Added Comment class in ApplicationBundle

---
+++ src/Sw/Bundle/ApplicationBundle/Entity/Comment.php
@@ -0,0 +1,74 @@
+<?php
+namespace Sw\Bundle\ApplicationBundle\Entity;
+
+/**
+ * Comment entity
+ */
+class Comment
+{
+    private $id;
+    private $author;
+    private $swimmingPoolId;
+    private $rank;
+    private $content;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getSwimmingPoolId()
+    {
+        return $this->swimmingPoolId;
+    }
+
+    public function setSwimmingPoolId($swimmingPoolId)
+    {
+        $this->swimmingPoolId = $swimmingPoolId;
+
+        return $this;
+    }
+
+    public function getRank()
+    {
+        return $this->rank;
+    }
+
+    public function setRank($rank)
+    {
+        $this->rank = $rank;
+
+        return $this;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
