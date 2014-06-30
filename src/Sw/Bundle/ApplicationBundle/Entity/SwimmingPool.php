1e0466d0edbf6b99c979696ad1f2c8edb36270e4
TheMechanic <j.dupontbillant@gmail.com>
2014-06-30 20:58:59 +0200

    add RestController + SwimmingPool class + new.html.twig

---
+++ src/Sw/Bundle/ApplicationBundle/Entity/SwimmingPool.php
@@ -0,0 +1,87 @@
+<?php
+namespace Sw\Bundle\ApplicationBundle\Entity;
+
+/**
+ * SwimmingPool entity
+ */
+class SwimmingPool
+{
+    private $id;
+    private $name;
+    private $address;
+    private $zipCode;
+    private $lat;
+    private $lon;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode($zipCode)
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLon()
+    {
+        return $this->lon;
+    }
+
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+}
